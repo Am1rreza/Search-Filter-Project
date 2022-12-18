43f07f45a5a0933e9d8f52faafcb9e777a14d06a
Amirreza <amirreza.m13825@gmail.com>
2022-12-18 12:10:20 +0330

    render products on DOM

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // get DOM elements
 const searchInput = document.querySelector("#search");
+const productsDOM = document.querySelector(".product-center");
 
 // global variables
 let allProductsData = [];
@@ -24,8 +25,27 @@ searchInput.addEventListener("input", (e) => {
   renderProducts(allProductsData, filters);
 });
 
+// functions
 function renderProducts(_products, _filters) {
   const filteredProducts = _products.filter((p) => {
     return p.title.toLowerCase().includes(_filters.searchItem.toLowerCase());
+  });
+  // clear DOM from last products
+  productsDOM.innerHTML = "";
+  // render to DOM
+  filteredProducts.forEach((item) => {
+    // create
+    const productsDiv = document.createElement("div");
+    productsDiv.classList.add("product");
+    // add content
+    productsDiv.innerHTML = `<div class="img-container">
+    <img class="product-img" src="${item.image}" alt="p-${item.id}" />
+  </div>
+  <div class="product-desc">
+    <p class="product-price">${item.price}$</p>
+    <p class="product-title">${item.title}</p>
+  </div>`;
+    // append to DOM
+    productsDOM.appendChild(productsDiv);
   });
 }
