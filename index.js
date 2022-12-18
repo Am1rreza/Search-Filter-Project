d1988c565ef8f18fedcf0d7cae3511224e7b2f79
Amirreza <amirreza.m13825@gmail.com>
2022-12-18 12:24:32 +0330

    filter products based on categories

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // get DOM elements
 const searchInput = document.querySelector("#search");
 const productsDOM = document.querySelector(".product-center");
+const buttons = [...document.querySelectorAll(".btn")];
 
 // global variables
 let allProductsData = [];
@@ -30,13 +31,16 @@ function renderProducts(_products, _filters) {
   const filteredProducts = _products.filter((p) => {
     return p.title.toLowerCase().includes(_filters.searchItem.toLowerCase());
   });
+
   // clear DOM from last products
   productsDOM.innerHTML = "";
+
   // render to DOM
   filteredProducts.forEach((item) => {
-    // create
+    // create tag
     const productsDiv = document.createElement("div");
     productsDiv.classList.add("product");
+
     // add content
     productsDiv.innerHTML = `<div class="img-container">
     <img class="product-img" src="${item.image}" alt="p-${item.id}" />
@@ -45,7 +49,16 @@ function renderProducts(_products, _filters) {
     <p class="product-price">${item.price}$</p>
     <p class="product-title">${item.title}</p>
   </div>`;
+
     // append to DOM
     productsDOM.appendChild(productsDiv);
   });
 }
+
+// filter based on groups
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    filters.searchItem = e.target.dataset.filter;
+    renderProducts(allProductsData, filters);
+  });
+});
